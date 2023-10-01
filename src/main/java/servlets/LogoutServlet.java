868ba2abf431c4f1a9fb502d922fe5040881a912
LaboratorioExprivia <118175982+LaboratorioExprivia@users.noreply.github.com>
2023-10-01 22:10:35 +0100

    Update LogoutServlet.java

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -1,28 +1,22 @@
-package servlets;
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Ottieni la sessione corrente
-        HttpSession session = request.getSession(false);
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("link.html").include(request, response);
+		HttpSession session = request.getSession();
+		session.invalidate();
+		out.print("You are successfully logged out!");
+		out.close();
+	}
 
-        if (session != null) {
-            // Invalida la sessione solo se esiste
-            session.invalidate();
-        }
-
-        // Reindirizza l'utente alla pagina di login o a una pagina di conferma del logout
-        response.sendRedirect("login.html"); // Modifica l'URL in base alla tua struttura di directory
-
-        // Alternativamente, puoi restituire un messaggio di conferma di logout
-    }
 }
