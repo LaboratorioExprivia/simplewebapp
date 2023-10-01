4adf8d6c92171b74c9766c5fde0cf008b68e8500
LaboratorioExprivia <118175982+LaboratorioExprivia@users.noreply.github.com>
2023-10-01 21:46:14 +0100

    Update LoginServlet.java

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -2,36 +2,40 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Sample servlet class for login related methods
- */
 public class LoginServlet extends HttpServlet {
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		request.getRequestDispatcher("link.html").include(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-		String userid = request.getParameter("userid");
-		String password = request.getParameter("password");
+        // Validazione dei parametri in ingresso
+        String userid = request.getParameter("userid");
+        String password = request.getParameter("password");
 
-		if (password.equals("admin")) {
-			out.print("Welcome, " + userid);
-			HttpSession session = request.getSession();
-			session.setAttribute("name", userid);
-		} else {
-			out.print("Sorry, username or password error!");
-			request.getRequestDispatcher("login.html").include(request,
-					response);
-		}
-		out.close();
-	}
+        if (userid != null && password != null && isValidUser(userid, password)) {
+            // Autenticazione riuscita, crea una sessione
+            HttpSession session = request.getSession(true);
+            session.setAttribute("name", userid);
 
+            // Reindirizza a una pagina di benvenuto
+            response.sendRedirect("welcome.jsp");
+        } else {
+            // Autenticazione fallita, restituisci un errore
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Autenticazione fallita");
+        }
+    }
+
+    // Metodo per la verifica delle credenziali (da implementare in modo sicuro)
+    private boolean isValidUser(String userid, String password) {
+        // Qui dovresti implementare la verifica sicura delle credenziali,
+        // ad esempio, confrontando con un database o utilizzando l'hashing delle password.
+        // Restituisci true se l'utente è valido, altrimenti false.
+        return userid.equals("admin") && password.equals("adminPassword");
+    }
 }
