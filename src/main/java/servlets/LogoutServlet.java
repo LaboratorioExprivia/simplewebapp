1b2fce6e70ceaf753156c38d411dabfbb002dd5a
LaboratorioExprivia <118175982+LaboratorioExprivia@users.noreply.github.com>
2024-11-10 15:52:47 +0000

    Update LogoutServlet.java

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LogoutServlet extends HttpServlet {
+/*public class LogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
@@ -22,3 +22,4 @@ public class LogoutServlet extends HttpServlet {
 	}
 
 }
+*/
